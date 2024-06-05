b43847703d89ba1f49db0f2dad92eeee286c69b3
sankalproy11 <sankalproy.dev@gmail.com>
2024-06-05 17:11:51 +0530

    rotate array right upto k places brute
    Signed-off-by: sankalproy11 <sankalproy.dev@gmail.com>

--- src/Arrays/RotateBykPlaces/Brute/Main.java
+++ src/Arrays/RotateBykPlaces/Brute/Main.java
@@ -3,7 +3,7 @@ package Arrays.RotateBykPlaces.Brute;
 import java.util.Scanner;
 
 public class Main {
-    public static void RotateByK(int[] arr, int k){
+    public static void RotateByKLeft(int[] arr, int k){
         k = k % arr.length;
         int[] temp = new int[k];
         for (int i = 0; i < k; i++) {
@@ -18,6 +18,20 @@ public class Main {
             j++;
         }
     }
+    public static void RotateByKRight(int[] arr, int k){
+        int n = arr.length;
+        k = k % n;
+        int[] temp = new int[n];
+        for (int i = n-k; i < n; i++) {
+            temp[i-(n-k)] = arr[i];
+        }
+        for (int i = k; i < n;  i++) {
+            temp[i] = arr[i-k];
+        }
+        for (int i = 0; i < n; i++) {
+            arr[i] = temp[i];
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
@@ -27,10 +41,19 @@ public class Main {
         for (int i = 0; i < inputArr.length; i++) {
             arr[i] = Integer.parseInt(inputArr[i]);
         }
-        
-        RotateByK(arr, k);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]+" ");
+        int[] arr2 = new int[inputArr.length];
+        System.arraycopy(arr, 0, arr2, 0, inputArr.length);
+
+        RotateByKLeft(arr, k);
+        RotateByKRight(arr2, k);
+        System.out.println("Rotated Left by "+k+" places");
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+        System.out.println("Rotated Right by "+k+" places");
+        for (int j : arr2) {
+            System.out.print(j + " ");
         }
     }
 }
