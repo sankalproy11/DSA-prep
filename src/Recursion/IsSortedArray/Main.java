17e1d6685ab390c3a53c831f4e9a3a0b5613dbea
sankalproy11 <sankalproy.dev@gmail.com>
2024-06-24 20:26:04 +0530

    is sorted array

---
+++ src/Recursion/IsSortedArray/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static boolean isSorted(int[] arr, int i){
+        if(i == arr.length - 1) return true;
+        if (arr[i]>arr[i+1]) return false;
+
+        return isSorted(arr, i+1);
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 1};
+        int i = 0;
+        System.out.println(isSorted(arr, i));
+    }
+}
