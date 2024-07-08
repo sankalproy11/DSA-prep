c7a601630cd3e7b9420ca144a96bdfe3b397c2b1
sankalproy11 <sankalproy.dev@gmail.com>
2024-07-08 16:28:27 +0530

    modified constructor

--- src/BinaryTree-I/LevelOrderTraversal/Main.java
+++ src/BinaryTree-I/LevelOrderTraversal/Main.java
@@ -7,6 +7,8 @@ public class Main{
         Node left, right;
 
         Node(int data){
+            this.left = null;
+            this.right = null;
             this.data = data;
         }
     }
